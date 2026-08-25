75f0a5bf5c22f20dc5900aa889402ff13c6d345f
Eshal <eshalnaeem2008@gmail.com>
2026-08-25 12:20:33 +0500

    add datatypes practice

---
+++ 01_Basics/datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string productName="Laptop";
+    int quantity=3;
+    double price=85000.0;
+    char category='E';
+    bool available=true;
+
+
+    cout<<"Product Name: "<<productName<<"\n";
+    cout<<"Quantity: "<<quantity<<"\n";
+    cout<<"Price: "<<price<<"\n";
+    cout<<"Category: "<<category<<"\n";
+    cout<<"Available: "<<available<<"\n";
+
+    return 0;
+
+
+}
