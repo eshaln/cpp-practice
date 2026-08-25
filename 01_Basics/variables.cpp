f9a55926b188115a5f55a4deb923a92b16be16df
Eshal <eshalnaeem2008@gmail.com>
2026-08-25 12:20:52 +0500

    add variables practice

---
+++ 01_Basics/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name="Eshal";
+    int age=18;
+    double height=5.4;
+    bool student=true;
+
+    age=19; //changing age
+    
+    cout<<"Name: "<< name << "\n" ;
+    cout<<"Age: "<< age << "\n";
+    cout<<"Height: "<< height << "\n";
+    cout<<"Student: "<<student;
+
+    return 0;
+
+}
