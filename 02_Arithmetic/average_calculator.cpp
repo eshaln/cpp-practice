251583db858dc34097a0bd15623757669bd6e279
Eshal <eshalnaeem2008@gmail.com>
2026-08-25 12:40:19 +0500

    feat: Add average calculator practice program

---
+++ 02_Arithmetic/average_calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double num1, num2 ,num3;
+
+    cout<<"Enter three numbers: ";
+    cin>>num1>>num2>>num3;
+
+    double average=(num1+num2+num3)/3;
+    cout<<"Average: "<<average<<"\n";
+    return 0;
+
+
+}
