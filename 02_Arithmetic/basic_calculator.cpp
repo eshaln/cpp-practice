e3bc1316db987751ad2dd43a6f110702eba9b411
Eshal <eshalnaeem2008@gmail.com>
2026-08-25 12:39:58 +0500

    feat: Add basic calculator practice program

---
+++ 02_Arithmetic/basic_calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    double num1;
+    double num2;
+
+    cout<<"Enter num1"<<"\n";
+    cin>>num1;
+
+    cout<<"Enter num2"<<"\n";
+    cin>>num2;
+
+    cout<<"Addition: "<<num1+num2<<"\n";
+    cout<<"Subtraction: "<<num1-num2<<"\n";
+    cout<<"Multiplication: "<<num1*num2<<"\n";
+    cout<<"Division: "<<num1/num2<<"\n";
+
+
+   return 0;
+}
