e2d07d345c5a2faf8e263204000dfb71eed00e5f
Eshal <eshalnaeem2008@gmail.com>
2026-08-31 12:45:51 +0500

    Add sum of numbers practice program using for loop

---
+++ 05_Loops/SumOfNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    int sum=0;
+    cout<<"Enter a number: ";
+    cin>>number;
+    for (int i=1;i<=number;i++) {
+        sum+=i;
+
+
+    }
+    cout<<"Sum of numbers: "<<sum<<endl;
+    return 0;
+
+}
