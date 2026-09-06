07a4c501de1ea1be5b2b3a67c6f1cc4a6fdf572d
Eshal <eshalnaeem2008@gmail.com>
2026-09-06 19:14:41 +0500

    Add SwapByReference.cpp practice program

---
+++ 06_Functions/SwapByReference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+int main() {
+    int x=20;
+    int y=40;
+    cout<<"Before swap: "<<x<<" "<<y<<endl;
+
+    swap(x, y);
+
+    cout<<"After swap: "<<x<<" "<<y<<endl;
+    return 0;
+
+}
